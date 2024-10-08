f0537929109cfb56de16e77ab1901e6ca9d9c20c
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-10-08 23:34:10 +0530

    feat 3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let recognition;
+let fullTranscript = '';
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
+const saveBtn = document.getElementById('saveBtn');
 const output = document.getElementById('output');
 
 if ('webkitSpeechRecognition' in window) {
@@ -14,13 +16,21 @@ if ('webkitSpeechRecognition' in window) {
 
         for (let i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
-                finalTranscript += event.results[i][0].transcript;
+                finalTranscript += event.results[i][0].transcript + ' ';
             } else {
                 interimTranscript += event.results[i][0].transcript;
             }
         }
 
-        output.innerHTML = finalTranscript + '<i style="color:#999">' + interimTranscript + '</i>';
+        fullTranscript += finalTranscript;
+        output.innerHTML = fullTranscript + '<i style="color:#999">' + interimTranscript + '</i>';
+        output.scrollTop = output.scrollHeight;
+    };
+
+    recognition.onend = () => {
+        if (startBtn.disabled) {
+            recognition.start();
+        }
     };
 
     recognition.onerror = (event) => {
@@ -28,18 +38,33 @@ if ('webkitSpeechRecognition' in window) {
     };
 
     startBtn.onclick = () => {
+        fullTranscript = '';
+        output.innerHTML = '';
         recognition.start();
         startBtn.disabled = true;
         stopBtn.disabled = false;
+        saveBtn.disabled = true;
     };
 
     stopBtn.onclick = () => {
         recognition.stop();
         startBtn.disabled = false;
         stopBtn.disabled = true;
+        saveBtn.disabled = false;
+    };
+
+    saveBtn.onclick = () => {
+        const blob = new Blob([fullTranscript], {type: 'text/plain'});
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'transcript.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
     };
 } else {
     startBtn.style.display = 'none';
     stopBtn.style.display = 'none';
+    saveBtn.style.display = 'none';
     output.innerHTML = 'Web Speech API is not supported in this browser.';
 }
